1f06ce0a158ec7ddbb1b2748f882a73a62e15274
Florian Frank <Florian.Frank@uni-passau.de>
2022-03-15 10:27:48 +0100

    Add comments to the ErrorCodes.h file.

--- Arduino_Firmware/Include/ErrorCodes.h
+++ Arduino_Firmware/Include/ErrorCodes.h
@@ -11,13 +11,21 @@ enum ErrorCode {
     NO_ERROR            = 0,
     /** Invalid index, e.g. invalid row or column is chosen. */
     INVALID_INDEX       = 1,
+    /** Message received from the UART interface is invalid. */
     INVALID_MESSAGE     = 2,
+    /** Interface is closed no actions can be executed. */
     INTERFACE_CLOSED    = 3,
+    /** TODO. */
     INVALID_LENGTH      = 4,
+    /** Length of input buffer is invalid. */
     INVALID_BUFFER_LEN  = 5,
+    /** Invalid argument passed to a function. */
     INVALID_ARGUMENT    = 6,
+    /** Command is invalid and will not be executed. */
     INVALID_COMMAND     = 7,
+    /** Device is disconnected. */
     DISCONNECTED        = 8,
+    /** Trying to execute a function on a not initialized structure. */
     NOT_INITIALIZED     = 9
 };
 
