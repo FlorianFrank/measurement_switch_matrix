a132567f0804b50320493a49c8e8ef5ec24535da
Florian Frank <Florian.Frank@uni-passau.de>
2022-03-15 10:24:50 +0100

    Add comments to the list of different commands.

--- Arduino_Firmware/Include/Commands.h
+++ Arduino_Firmware/Include/Commands.h
@@ -7,12 +7,19 @@
 #define NANOSEC_CROSSBAR_CONTROLLER_COMMANDS_H
 
 enum Command {
+    /** Establish a connection to the Arduino Nano. */
     CONNECT         = 0,
+    /** Disconnect connection from the Arduino Nano. */
     DISCONNECT      = 1,
+    /** Select and switch a ro specified by its ID. */
     SELECT_ROW      = 2,
+    /** Select and switch a column specified by its ID. */
     SELECT_COLUMN   = 3,
+    /** Send an acknowledgement that a certain command was executed. */
     ACK             = 4,
+    /** Send error code if an error occurred. */
     ERROR_CODE      = 5,
+    /** No command executed. */
     NOPE            = 6
 };
 
