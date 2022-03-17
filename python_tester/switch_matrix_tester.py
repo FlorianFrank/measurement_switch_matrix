b9c0c162c2d685147125d357c0ffbb6401b84c8e
Florian Frank <Florian.Frank@uni-passau.de>
2022-03-17 11:49:04 +0100

    Add timeout and loggign behavior to python sample

--- python_tester/switch_matrix_tester.py
+++ python_tester/switch_matrix_tester.py
@@ -4,71 +4,103 @@ import serial
 import time
 import signal
 import sys
+import logging
 
 # Is true until CTRL + C is pressed which exits the program.
 running = True
 
 
-def connect_to_arduino(serial_port):
+def connect_to_arduino(serial_port, number_of_attempts=3):
     """
     Sends a connection request to the Arduino Nano as long as the connection is established.
     Args:
         serial_port: previously opened serial port to connect to the Arduino.
     """
     send_connect_message(serial_port)
-    ret = serial_port.readline()
-    while len(ret) == 0:
-        send_connect_message(serial_port)
-        ret = serial_port.readline()
+    for i in range(number_of_attempts):
+        print("Try to connect (attempt {})".format(i))
+        if send_connect_message(serial_port):
+            print("Successfully connected")
+            return True
+        else:
+            print("Timeout when trying to connect")
 
 
-def send_connect_message(serial_port):
+def wait_for_response(serial_port, timeout_in_secs):
+    time.sleep(1)
+    start_ts = time.time()
+    while time.time() < start_ts + timeout_in_secs:
+        ack = serial_port.readline()
+        logging.info("readlines returned: {}".format(ack))
+        ack = ack.decode('ascii')
+        if 'ack' in ack:
+            print(ack)
+            serial_port.flush()
+            return True
+        if 'error' in ack:
+            print(ack)
+            return False
+    return False
+
+
+def send_connect_message(serial_port, timeout_in_secs=2):
     """
     Sends the connect message to the Arduino Nano.
     Args:
         serial_port: previously opened serial port to connect to the Arduino.
+        timeout_in_secs: Timeout in seconds to wait for the acknowledgement.
     """
-    return serial_port.write('connect\n'.encode('ascii'))
+    serial_port.write('connect\n'.encode('ascii'))
+    return wait_for_response(serial_port, timeout_in_secs)
 
 
-def send_disconnect_message(serial_port):
+def send_disconnect_message(serial_port, timeout_in_secs=2):
     """
     Sends the disconnect message to the Arduino Nano.
     Args:
         serial_port: previously opened serial port to disconnect from the Arduino.
+        timeout_in_secs: Timeout in seconds to wait for the acknowledgement.
     """
-    return serial_port.write('disconnect\n'.encode('ascii'))
+    serial_port.write(b'disconnect\n')
+    wait_for_response(serial_port, timeout_in_secs)
+    serial_port.close()
 
 
-def write_row(serial_port, row_idx):
+def write_row(serial_port, row_idx, timeout_in_secs=2):
     """
     Sends r: <row idx> to the Arduino nano
     Args:
         serial_port: previously opened serial port to connect to the Arduino.
         row_idx: row to set valid values are 0 - 12. 12 means no row is selected.
+        timeout_in_secs: Timeout in seconds to wait for the acknowledgement.
     """
     row = 'r:{}\n'.format(row_idx)
     serial_port.write(row.encode('ascii'))
+    logging.info("Write {}".format(row.encode('ascii')))
+    return wait_for_response(serial_port, timeout_in_secs)
 
 
-def write_column(serial_port, column_idx):
+def write_column(serial_port, column_idx, timeout_in_secs=2):
     """
      Sends c: <column idx> to the Arduino nano
      Args:
          serial_port: previously opened serial port to connect to the Arduino.
          column_idx: column to set valid values are 0 - 12. 12 means no column is selected.
+         timeout_in_secs: Timeout in seconds to wait for the acknowledgement.
      """
     column = 'c:{}\n'.format(column_idx)
     serial_port.write(column.encode('ascii'))
+    logging.info("Write {}".format(column.encode('ascii')))
+    return wait_for_response(serial_port, timeout_in_secs)
 
 
-def signal_handler():
+def signal_handler(not_used_signal, not_used_arg):
     """
     Sets running to False, disconnects from the Arduino and exits the program.
     """
     global running
     running = False
-    printf("CTRL + C received -> exit program")
+    print("CTRL + C received -> exit program")
 
 
 def main(interface):
@@ -76,19 +108,26 @@ def main(interface):
     Main function continuously iterates over all rows and columns.
     Sleeps 0.1s between each row and column selection.
     """
+    global running
+    logging.basicConfig(level=logging.INFO)
     signal.signal(signal.SIGINT, signal_handler)
-    with serial.Serial(interface, 9600, timeout=2, parity=serial.PARITY_NONE) as serial_port:
+    with serial.Serial(interface, 9600, timeout=2) as serial_port:
+        serial_port.flush()
         print("Connect to board")
         connect_to_arduino(serial_port)
 
         while running:
             for row_ctr in range(12):
+                if not running:
+                    break
                 write_row(serial_port, row_ctr)
-                time.sleep(0.1)
+                time.sleep(1)
                 for column_ctr in range(12):
+                    if not running:
+                        break
                     print("Select Row: {} Column: {}".format(row_ctr, column_ctr))
                     write_column(serial_port, column_ctr)
-                    time.sleep(0.1)
+                    time.sleep(1)
         send_disconnect_message(serial_port)
 
 
