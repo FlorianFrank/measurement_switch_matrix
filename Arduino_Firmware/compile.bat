f97cf10a434a38ba7172b8c1f2bd41cfe775888f
Florian Frank <Florian.Frank@uni-passau.de>
2022-03-24 09:41:58 +0100

    Add relative path of cmake to compile.bat

--- Arduino_Firmware/compile.bat
+++ Arduino_Firmware/compile.bat
@@ -3,7 +3,7 @@ set build_dir=%cd%\build
 echo "Create build folder: %build_dir% if not exists"
 mkdir %build_dir% 2> NUL
 pushd build
-cmake -GNinja .. 
+%cd%\..\Toolchain\cmake-3.23.0-rc5-windows-x86_64\bin\cmake.exe -GNinja .. 
 %cd%\..\Toolchain\Ninja\ninja.exe
 popd
 
