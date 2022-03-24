c6b420034359c9957ed26dc9c950f6188928cb5f
Florian Frank <Florian.Frank@uni-passau.de>
2022-03-24 10:02:34 +0100

    Add build script for linux

---
+++ switch_matrix_uart_lib/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='switch_matrix_uart_lib',
+    version='1.0',
+    packages=['tests', 'switch_matrix_uart_lib'],
+    url='',
+    license='MIT',
+    author='University of Passau',
+    author_email='Florian.Frank@uni-passau.de',
+    description='Library to access the NanoSEC switch matrix using over UART.'
+)
