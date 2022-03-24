3d19c7f30b58de2bca5f98fbfae362ecb81a8222
Florian Frank <Florian.Frank@uni-passau.de>
2022-03-24 10:03:02 +0100

    Refactor build settings into settings.cmake

---
+++ Arduino_Firmware/settings.cmake
@@ -0,0 +1,4 @@
+option(BUILD_DOCUMENTATION "BUILD_DOCUMENTATION" OFF)
+option(ARDUINO_NANO_OLD "BUILD for old Arduino Nano Board (blue)" ON)
+option(USB_PORT "Set the USB port on Linux /dev/ttyUSB0 on windows COM<comID>" "COM3")
+option(Programmer "Set the programmer to be used to flash the Arduino Nano" "avrispmkii")
