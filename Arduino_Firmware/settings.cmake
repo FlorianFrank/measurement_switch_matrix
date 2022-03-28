d8915c14a70ecf53460ef0d3759d4bfddee06fc6
Florian Frank <Florian.Frank@uni-passau.de>
2022-03-28 12:02:13 +0200

    Adjust CMakeLists to allow custom upload target

--- Arduino_Firmware/settings.cmake
+++ Arduino_Firmware/settings.cmake
@@ -1,4 +1,7 @@
 option(BUILD_DOCUMENTATION "BUILD_DOCUMENTATION" OFF)
-option(ARDUINO_NANO_OLD "BUILD for old Arduino Nano Board (blue)" ON)
-option(USB_PORT "Set the USB port on Linux /dev/ttyUSB0 on windows COM<comID>" "COM3")
-option(Programmer "Set the programmer to be used to flash the Arduino Nano" "avrispmkii")
+option(ARDUINO_NANO_OLD "BUILD for old Arduino Nano Board (blue)" OFF)
+
+set(ARDUINO_BOARD_TYPE "nano") # Type like micro or nano
+set(USB_PORT  "/dev/ttyUSB0") # "Set the USB port on Linux /dev/ttyUSB0 on windows COM<comID>"
+set(ARDUINO_PROGRAMMER  "avrispmkii") # "Set the programmer to be used to flash the Arduino Nano"
+set(CMAKE_VERBOSE_MAKEFILE on)
