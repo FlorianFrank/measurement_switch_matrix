5fd3d0c0275d9f62c63fc913413164778a868bcc
Florian Frank <Florian.Frank@uni-passau.de>
2023-10-10 12:44:04 +0200

    Add ethernet support to CMakeLists.txt

--- Arduino_Firmware/settings.cmake
+++ Arduino_Firmware/settings.cmake
@@ -4,4 +4,5 @@ option(ARDUINO_NANO_OLD "BUILD for old Arduino Nano Board (blue)" OFF)
 set(ARDUINO_BOARD_TYPE "nano") # Type like micro or nano
 set(USB_PORT  "/dev/ttyUSB0") # "Set the USB port on Linux /dev/ttyUSB0 on windows COM<comID>"
 set(ARDUINO_PROGRAMMER  "avrispmkii") # "Set the programmer to be used to flash the Arduino Nano"
-set(CMAKE_VERBOSE_MAKEFILE on)
+set(CMAKE_VERBOSE_MAKEFILE ON)
+option(ETHERNET_SUPPORT "Build UIPEthernet Lib" ON)
