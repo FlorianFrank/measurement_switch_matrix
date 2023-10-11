5847ba826c19f0a048a40794e94a34267b3537fa
Florian Frank <Florian.Frank@uni-passau.de>
2023-10-11 12:26:41 +0200

    Cleanup code of ethernet tester

--- Arduino_Firmware/ethernet_tester/ethernet_tester.py
+++ Arduino_Firmware/ethernet_tester/ethernet_tester.py
@@ -1,19 +1,30 @@
-# echo-client.py
-
 import socket
 import json
 import time
 
 HOST = "192.168.0.2"
 PORT = 2000
+TIMEOUT = 5  # 5 seconds
 
-json_string = ['{"cmd":"*IDN?"}\0', '{"cmd":"SET_ROW","row":"4"}\0', '{"cmd":"SET_COLUMN","column":"10"}\0', '{"cmd":"SET_ROW_COLUMN","row":"1","column":"2"}\0']
+json_strings = [
+    {"cmd": "*IDN?"},
+    {"cmd": "SET_ROW", "row": "4"},
+    {"cmd": "SET_COLUMN", "column": "10"},
+    {"cmd": "SET_ROW_COLUMN", "row": "1", "column": "2"},
+]
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    for i in json_string:
-        print("Send " + i)
-        s.sendall(i.encode())
-        data = s.recv(60)
-        print(data)
-        time.sleep(1)
+    try:
+        s.connect((HOST, PORT))
+        s.settimeout(TIMEOUT)  # Set a timeout for socket operations
+
+        for json_data in json_strings:
+            json_string = json.dumps(json_data)
+            print("Send:", json_string)
+            s.sendall(json_string.encode())
+            data = s.recv(60)
+            print("Received:", data)
+            time.sleep(1)
+    except (socket.timeout, ConnectionError) as e:
+        print("Error:", e)
+
