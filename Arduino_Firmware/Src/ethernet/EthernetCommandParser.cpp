a509ce8bd7fd87a0f0636d77bf4179b9aa2bd017
Alexander Braml <braml@fim.uni-passau.de>
2023-10-27 14:07:06 +0200

    Fix memory leak

--- Arduino_Firmware/Src/ethernet/EthernetCommandParser.cpp
+++ Arduino_Firmware/Src/ethernet/EthernetCommandParser.cpp
@@ -4,8 +4,8 @@
 #include <string.h>
 
 #define IDENT_RESPONSE R"({"status":"ok","idn":"NANOSec Crossbar Controller"})"
-#define SET_ROW_RESPONSE R"({"status":"ok","row":"%d"})"
-#define SET_COLUMN_RESPONSE R"({"status":"ok","column":"%d"})"
+#define SET_ROW_RESPONSE R"({"status":"ok","row":%d})"
+#define SET_COLUMN_RESPONSE R"({"status":"ok","column":%d})"
 #define SET_ROW_COLUMN_RESPONSE R"({"status":"ok","row":%d,"column":%d})"
 
 #define PARSE_CMD_ERR_RESPONSE R"({"status":"err","msg":"Could not parse cmd"})"
@@ -19,8 +19,8 @@
  * @brief This array contains all commands which are supported by the ethernet interface.
  */
 /*static*/ char EthernetCommandParser::cmdList[NR_COMMANDS][MAX_CMD_LEN] = {"*IDN?", "SET_ROW",
-                                                                                         "SET_COLUMN",
-                                                                                         "SET_ROW_COLUMN"};
+                                                                            "SET_COLUMN",
+                                                                            "SET_ROW_COLUMN"};
 
 /**
  * @brief This method extracts the value of a json element by its key.
@@ -28,18 +28,18 @@
  * @param key the key to filter, e.g. "cmd" or "row".
  * @return The corresponding value of the key, e.g. "SET_ROW_COLUMN" or "1".
  */
-const char* extractJSONValueByKey(const char* jsonString, const char* key) {
-    const char* cmdValue = nullptr;
+void extractJSONValueByKey(const char *jsonString, const char *key, char *extractedKey) {
+    const char *cmdValue = nullptr;
 
     // Find the position of the "key" field
-    const char* keyPos = strstr(jsonString, key);
+    const char *keyPos = strstr(jsonString, key);
 
     if (keyPos != nullptr) {
         // Find the starting position of the value (after the colon and any whitespace)
         keyPos = strstr(keyPos, ":");
         if (keyPos != nullptr) {
             keyPos++;
-            while (isspace((unsigned char)*keyPos)) {
+            while (isspace((unsigned char) *keyPos)) {
                 keyPos++;
             }
 
@@ -51,31 +51,30 @@ const char* extractJSONValueByKey(const char* jsonString, const char* key) {
                     // Calculate the length of the extracted value
                     size_t cmdLength = keyPos - cmdValue;
                     // Allocate memory and copy the value
-                    char* extractedKey = (char*)malloc(cmdLength + 1);
                     if (extractedKey != nullptr) {
                         strncpy(extractedKey, cmdValue, cmdLength);
                         extractedKey[cmdLength] = '\0';
-                        return extractedKey;
+                    } else {
+                        Serial.println(F("Malloc returned nullptr!"));
                     }
                 }
-            } else if (isdigit((unsigned char)*keyPos) || *keyPos == '-') {
+            } else if (isdigit((unsigned char) *keyPos) || *keyPos == '-') {
                 // Numeric value (integer or float)
-                const char* endPos = keyPos;
-                while (*endPos && (isdigit((unsigned char)*endPos) || *endPos == '.' || *endPos == '-' || *endPos == 'e' || *endPos == 'E')) {
+                const char *endPos = keyPos;
+                while (*endPos &&
+                       (isdigit((unsigned char) *endPos) || *endPos == '.' || *endPos == '-' || *endPos == 'e' ||
+                        *endPos == 'E')) {
                     endPos++;
                 }
                 size_t cmdLength = endPos - keyPos;
                 // Allocate memory and copy the numeric value
-                char* extractedKey = (char*)malloc(cmdLength + 1);
                 if (extractedKey != nullptr) {
                     strncpy(extractedKey, keyPos, cmdLength);
                     extractedKey[cmdLength] = '\0';
-                    return extractedKey;
                 }
             }
         }
     }
-    return nullptr;
 }
 
 /**
@@ -86,41 +85,47 @@ const char* extractJSONValueByKey(const char* jsonString, const char* key) {
  * @param column Parsed column returned when the command is SET_COLUMN or SET_ROW_COLUMN.
  * @return Parsed command.
  */
-/*static*/ Command EthernetCommandParser::ParseCommand(char *command, char *response, int *row, int *column)
-{
-    Serial.println(command);
-    const char* cmdValue = extractJSONValueByKey(command, "cmd");
-    if(!cmdValue){
+/*static*/ Command EthernetCommandParser::ParseCommand(char *command, char *response, int *row, int *column) {
+    char *cmdValue = (char *) malloc(14);
+    extractJSONValueByKey(command, "cmd", cmdValue);
+
+    Command ret;
+    if (!cmdValue) {
         strcpy(response, PARSE_CMD_ERR_RESPONSE);
-        return ERROR_CODE;
+        ret = ERROR_CODE;
     }
 
-    if(strcmp(cmdValue, cmdList[IDN]) == 0){
+    if (strcmp(cmdValue, cmdList[IDN]) == 0) {
         strcpy(response, IDENT_RESPONSE);
-        return IDENT;
-    } else if(strcmp(cmdValue, cmdList[SET_ROW]) == 0){
-        if(ParseRowColumnIdentifier(command, SET_ROW, response, row, nullptr) != NO_ERROR){
+        ret = IDENT;
+    } else if (strcmp(cmdValue, cmdList[SET_ROW]) == 0) {
+        if (ParseRowColumnIdentifier(command, SET_ROW, response, row, nullptr) != NO_ERROR) {
             strcpy(response, RESPONSE_JSON_PARSE_ERR);
-            return ERROR_CODE;
+            ret = ERROR_CODE;
+        } else {
+            ret = SELECT_ROW;
         }
-        return SELECT_ROW;
-    }else if(strcmp(cmdValue, cmdList[SET_COLUMN]) == 0){
-        Serial.println(cmdValue);
-
-        if(ParseRowColumnIdentifier(command, SET_COLUMN, response, nullptr, column) != NO_ERROR){
+    } else if (strcmp(cmdValue, cmdList[SET_COLUMN]) == 0) {
+        if (ParseRowColumnIdentifier(command, SET_COLUMN, response, nullptr, column) != NO_ERROR) {
             strcpy(response, RESPONSE_JSON_PARSE_ERR);
-            return ERROR_CODE;
+            ret = ERROR_CODE;
+        } else {
+            ret = SELECT_COLUMN;
         }
-        return SELECT_COLUMN;
-    }else if(strcmp(cmdValue, cmdList[SET_ROW_COLUMN]) == 0){
-        if(ParseRowColumnIdentifier(command, SET_ROW_COLUMN, response, row, column) != NO_ERROR){
+    } else if (strcmp(cmdValue, cmdList[SET_ROW_COLUMN]) == 0) {
+        if (ParseRowColumnIdentifier(command, SET_ROW_COLUMN, response, row, column) != NO_ERROR) {
             strcpy(response, RESPONSE_JSON_PARSE_ERR);
-            return ERROR_CODE;
+            ret = ERROR_CODE;
+        } else {
+            ret = SELECT_ROW_COLUMN;
         }
-        return SELECT_ROW_COLUMN;
-    }
+    } else {
         sprintf(response, UNKNOWN_CMD_ERR_RESPONSE, cmdValue);
-        return ERROR_CODE;
+        ret = ERROR_CODE;
+    }
+
+    free(cmdValue);
+    return ret;
 }
 
 /**
@@ -132,56 +137,76 @@ const char* extractJSONValueByKey(const char* jsonString, const char* key) {
  */
 /*static*/ ErrorCode
 EthernetCommandParser::ParseRowColumnIdentifier(const char *jsonStr, IdentifierCMD identifier, char *response, int *row,
-                                                int *column)
-{
-    auto StrToInt = [](const char *strToTransform, int* result, char* response) -> ErrorCode {
+                                                int *column) {
+    auto StrToInt = [](const char *strToTransform, int *result, char *response) -> ErrorCode {
         char *endPtr;
-        Serial.println(strToTransform);
         *result = static_cast<int>(strtol(strToTransform, &endPtr, INT_TRANSFORM_BASE));
-        if (*endPtr != '\0' && !isspace((unsigned char)*endPtr)) {
+        if (*endPtr != '\0' && !isspace((unsigned char) *endPtr)) {
             strcpy(response, RESPONSE_JSON_PARSE_ERR);
             return JSON_PARSING_ERROR;
         }
         return NO_ERROR;
     };
 
-    switch (identifier)
-    {
-        case SET_ROW:
-        {
-            const char *rowValue = extractJSONValueByKey(jsonStr, ROW_IDENTIFIER);
-            if (!rowValue)
-                return JSON_PARSING_ERROR;
-            auto ret = StrToInt(rowValue, row, response);
-            if (ret == NO_ERROR)
-                sprintf(response, SET_ROW_RESPONSE, *row);
-            return ret;
-        }
-        case SET_COLUMN:
-        {
-            const char *columnValue = extractJSONValueByKey(jsonStr, COLUMN_IDENTIFIER);
-            if (!columnValue)
-                return JSON_PARSING_ERROR;
-            auto ret = StrToInt(columnValue, column, response);
-            if (ret == NO_ERROR)
-                sprintf(response, SET_COLUMN_RESPONSE, *column);
-            return ret;
-        }
-        case SET_ROW_COLUMN:
-        {
-            const char *rowValue = extractJSONValueByKey(jsonStr, ROW_IDENTIFIER);
-            if (!rowValue)
-                return JSON_PARSING_ERROR;
-            auto ret = StrToInt(rowValue, row, response);
-            if (ret != NO_ERROR)
-                return ret;
+    switch (identifier) {
+        case SET_ROW: {
+            char *rowValue = (char*) malloc(2);
+            extractJSONValueByKey(jsonStr, ROW_IDENTIFIER, rowValue);
 
-            const char *columnValue = extractJSONValueByKey(jsonStr, COLUMN_IDENTIFIER);
-            if (!columnValue)
+            ErrorCode ret;
+            if (!rowValue) {
+                ret = JSON_PARSING_ERROR;
+            } else {
+                ret = StrToInt(rowValue, row, response);
+                if (ret == NO_ERROR) {
+                    sprintf(response, SET_ROW_RESPONSE, *row);
+                }
+            }
+            free(rowValue);
+            return ret;
+        }
+        case SET_COLUMN: {
+            char *columnValue = (char*) malloc(2);
+            extractJSONValueByKey(jsonStr, COLUMN_IDENTIFIER, columnValue);
+            ErrorCode ret;
+            if (!columnValue) {
+                ret = JSON_PARSING_ERROR;
+            } else {
+                ret = StrToInt(columnValue, column, response);
+                if (ret == NO_ERROR) {
+                    sprintf(response, SET_COLUMN_RESPONSE, *column);
+                }
+            }
+            free(columnValue);
+            return ret;
+        }
+        case SET_ROW_COLUMN: {
+            char *rowValue = (char*) malloc(2);
+            extractJSONValueByKey(jsonStr, ROW_IDENTIFIER, rowValue);
+            ErrorCode ret;
+            if (!rowValue) {
+                free(rowValue);
                 return JSON_PARSING_ERROR;
-            ret = StrToInt(columnValue, column, response);
-            if (ret == NO_ERROR)
-                sprintf(response, SET_ROW_COLUMN_RESPONSE, *row, *column);
+            } else {
+                ret = StrToInt(rowValue, row, response);
+                if (ret != NO_ERROR) {
+                    free(rowValue);
+                    return ret;
+                }
+            }
+
+            char *columnValue = (char*) malloc(2);
+            extractJSONValueByKey(jsonStr, COLUMN_IDENTIFIER, columnValue);
+            if (!columnValue) {
+                ret = JSON_PARSING_ERROR;
+            } else {
+                ret = StrToInt(columnValue, column, response);
+                if (ret == NO_ERROR) {
+                    sprintf(response, SET_ROW_COLUMN_RESPONSE, *row, *column);
+                }
+            }
+            free(rowValue);
+            free(columnValue);
             return ret;
         }
     }
